e3c6117860ef753b2620b6aa3ff60aa9c1da3677
liellnima <jkaltenborn@uos.de>
2022-04-04 20:25:39 -0400

    users can plug in their individual data paths here. all hard coded pathways in the other files are replaced with these global paths

---
+++ path_parameters.py
@@ -0,0 +1,6 @@
+# CONFIGURE ME
+TRAIN_PATH = "~/Documents/Master/CausalSuperEmulator/Data/train_val/"
+TEST_PATH = "~/Documents/Master/CausalSuperEmulator/Data/test/"
+OUTPUT_PATH = "~/Documents/Master/CausalSuperEmulator/Data/outputs/"
+
+# TODO add main method to set paths via command line
